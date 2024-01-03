53e9d88213ee8c9b0dd585289c51cf650b5b09f2
benoitsafari <gregory.michalak.192@gmail.com>
2024-01-03 09:02:50 +0100

    docker compose config

--- scripts/init_server.sh
+++ scripts/init_server.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 
-# Install dependencies
+echo "Installing Dependencies..."
 sudo apt update
 sudo apt install -y ca-certificates curl gnupg
 
-# Download and install Docker's official GPG key
+echo "Installing Docker's official GPG key..."
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-# Add Docker's repository to Apt sources
+echo "Adding Docker's repository to Apt sources..."
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Install Docker and Nginx
+echo "Installing Docker and Nginx..."
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin nginx
 
-# Install Certbot
+echo "Installing certbot..."
 sudo snap install -y --classic certbot
 
-# Enable/Start services
+echo "Enabling/Starting services..."
 echo "y" | sudo ufw enable
 sudo systemctl start nginx
 
-# Allow OpenSSH, Nginx HTTP and Nginx HTTPS through UFW
+echo "Allowing OpenSSH, Nginx HTTP and Nginx HTTPS through UFW..."
 sudo ufw allow 'OpenSSH'
 sudo ufw allow 'Nginx HTTP'
 sudo ufw allow 'Nginx HTTPS'
 
-# Set initial nginx configuration
+echo "Copiying initial nginx configuration..."
 sudo rm -rf etc/nginx/sites-enabled/default
 sudo cp ./nginx/initial.conf /etc/nginx/conf.d/default.conf
-echo "Initial nginx configuration copied successfully"
 
-##############################################
-# TODO Initial Cinevoraces installation
-##############################################
+echo "Downloading cinevoraces..."
+git clone https://github.com/Cinevoraces/cinevoraces.git
 
-# Install certificates
+echo "Building cinevoraces..."
+docker compose up -d
+
+echo "Installing certificates..."
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
 sudo certbot --nginx
